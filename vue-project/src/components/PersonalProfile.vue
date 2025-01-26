7bae163917d099336296d75742c9337554e0535d
Jenine Dulay <158535998+jenenerz@users.noreply.github.com>
2025-01-26 17:34:09 +0800

    updated

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,106 +1,103 @@
-
 <template>
-<body>
-  <header class="navbar">
-    <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/470051954_596240062794812_7437720925054364491_n.png?stp=dst-png_s480x480&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=_NmsTlLHgy4Q7kNvgF32-a-&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gE9dWms0zgTIT_zYIGyX6pv4Pugm_rP-md3jfQAj_wIag&oe=67BD7D15" alt="logo" class="logo">
-    <ul>
-      <li><a href="#intro">home</a></li>
-      <li><a href="#education">acads</a></li>
-      <li><a href="#goals">more</a></li>
-    </ul>
-  </header>
-
-  <section id="intro" class="container">
-    <div class="intro">
-      <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/470852203_2741293039376032_1118496279224145919_n.png?stp=dst-png_s600x600&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=bhT7vdaXu4IQ7kNvgFgYpBZ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gGG5hlXmxGV_1CR4_Ep-HS93fAl3ZbZarKiOTDwgxGFZA&oe=67BD7A08" alt="intro-pic">
-      <div>
-        <h2>HELLO! I'M JENINE</h2>
-        <p>I am a 2nd year student currently taking Bachelor of Science in Information Technology with specialization in Mobile and Internet Technologies. I am 20 years old and I love music and froyo!!
-        </p>
-        <br>
-        <p>Add me on my socials!<br>
-          <a href="https://www.linkedin.com/in/jenine-dulay-b9988b284/" target="_blank">LinkedIn</a> |
-          <a href="https://open.spotify.com/user/parkjimin3389?si=717bf162501242f3" target="_blank">Spotify</a>
-        </p>
-      </div>
-    </div>
-  </section>
-
-  <section id="education" class="container">
-    <h2 align="center">EDUCATION / ACHIEVEMENTS</h2>
-    <p><strong>Immaculate Heart of Mary College (2018-2023)</strong></p>
-    <ul>
-      <li>Junior High - Senior High (STEM Strand)</li>
-      <li>Graduated Junior High with Honors</li>
-      <li>Graduated Senior High with Honors</li>
-      <li>STEM SOCIETY Organization Member</li>
-    </ul>
-    <br>
-    <p><strong>Asia Pacific College (2023-PRESENT)</strong></p>
-    <ul>
-      <li>BSIT-MI</li>
-      <li>1st Year of College - Honors</li>
-      <li>Cinemasoma Member</li>
-      <li>Flickers Photo Society Member</li>
-    </ul>
-  </section>
-
-  <section id="it-experience" class="container two-columns">
-    <div class="column">
-      <h2 align="center">IT EXPERIENCE</h2>
+  <div>
+    <header class="navbar">
+      <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/470051954_596240062794812_7437720925054364491_n.png?stp=dst-png_s480x480&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=_NmsTlLHgy4Q7kNvgF32-a-&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gE9dWms0zgTIT_zYIGyX6pv4Pugm_rP-md3jfQAj_wIag&oe=67BD7D15" alt="logo" class="logo">
       <ul>
-        <li>Basic Java</li>
-        <li>Basic Python</li>
-        <li>Had HTML during grade school</li>
-        <li>UI/UX Experience (Website and mobile: RamQuest Group Project)</li>
-        <li>Mobile Application (RESCUE app - Group Project)</li>
+        <li><a href="#intro">home</a></li>
+        <li><a href="#education">acads</a></li>
+        <li><a href="#goals">more</a></li>
       </ul>
-    </div>
-    <div class="column">
+    </header>
+
+    <section id="intro" class="container">
+      <div class="intro">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/470852203_2741293039376032_1118496279224145919_n.png?stp=dst-png_s600x600&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=bhT7vdaXu4IQ7kNvgFgYpBZ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gGG5hlXmxGV_1CR4_Ep-HS93fAl3ZbZarKiOTDwgxGFZA&oe=67BD7A08" alt="intro-pic">
+        <div>
+          <h2>HELLO! I'M JENINE</h2>
+          <p>I am a 2nd year student currently taking Bachelor of Science in Information Technology with specialization in Mobile and Internet Technologies. I am 20 years old and I love music and froyo!!</p>
+          <br>
+          <p>Add me on my socials!<br>
+            <a href="https://www.linkedin.com/in/jenine-dulay-b9988b284/" target="_blank">LinkedIn</a> |
+            <a href="https://open.spotify.com/user/parkjimin3389?si=717bf162501242f3" target="_blank">Spotify</a>
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section id="education" class="container">
+      <h2 align="center">EDUCATION / ACHIEVEMENTS</h2>
+      <p><strong>Immaculate Heart of Mary College (2018-2023)</strong></p>
+      <ul>
+        <li>Junior High - Senior High (STEM Strand)</li>
+        <li>Graduated Junior High with Honors</li>
+        <li>Graduated Senior High with Honors</li>
+        <li>STEM SOCIETY Organization Member</li>
+      </ul>
+      <br>
+      <p><strong>Asia Pacific College (2023-PRESENT)</strong></p>
+      <ul>
+        <li>BSIT-MI</li>
+        <li>1st Year of College - Honors</li>
+        <li>Cinemasoma Member</li>
+        <li>Flickers Photo Society Member</li>
+      </ul>
+    </section>
+
+    <section id="it-experience" class="container two-columns">
+      <div class="column">
+        <h2 align="center">IT EXPERIENCE</h2>
+        <ul>
+          <li>Basic Java</li>
+          <li>Basic Python</li>
+          <li>Had HTML during grade school</li>
+          <li>UI/UX Experience (Website and mobile: RamQuest Group Project)</li>
+          <li>Mobile Application (RESCUE app - Group Project)</li>
+        </ul>
+      </div>
+      <div class="column">
+        <div class="gallery">
+          <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/473012743_1014189447183586_8511567724061474199_n.png?stp=dst-png_s480x480&_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_ohc=htFVh6qWIT4Q7kNvgFanv7P&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gGsUuuUENqEV393JBJTv6hAVFzzE8WisjIsFLsOamJV0w&oe=67BD81EF" alt="IT Project 1">
+        </div>
+      </div>
+    </section>
+
+    <section id="goals" class="container two-columns">
+      <div class="column">
+        <h2 align="center">HOBBIES & INTERESTS</h2>
+        <ul>
+          <li>Music (1D, Chappell Roan, Olivia Rodrigo, SZA)</li>
+          <li>Films (Beautiful Boy, Challengers, Pitch Perfect Trilogy)</li>
+          <li>Shows (Bojack Horseman, Grey’s Anatomy, The Bear)</li>
+          <li>Video Games (Life is Strange, The Last of Us, Valorant)</li>
+          <li>Photography</li>
+        </ul>
+      </div>
+      <div class="column">
+        <h2 align="center">GOALS IN LIFE / DREAMS</h2>
+        <ul>
+          <li>To be able to live on my own and give back to my siblings</li>
+          <li>To be able to meet my favorite artists</li>
+          <li>Graduate college with Honors</li>
+          <li>Learn to play the piano</li>
+          <li>Find a stable job that I enjoy</li>
+          <li>Be able to drive without getting anxious</li>
+          <li>To live near a beach and be able to travel all around the world</li>
+        </ul>
+      </div>
+    </section>
+
+    <section id="gallery" class="container">
+      <h2 align="center">PICTURE GALLERY</h2>
       <div class="gallery">
-        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/473012743_1014189447183586_8511567724061474199_n.png?stp=dst-png_s480x480&_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_ohc=htFVh6qWIT4Q7kNvgFanv7P&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gGsUuuUENqEV393JBJTv6hAVFzzE8WisjIsFLsOamJV0w&oe=67BD81EF" alt="IT Project 1">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462577563_1221159922506811_5912691345356275466_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=FgwusuDZknsQ7kNvgH7m_Sw&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFS1l0vDk20hJ_-SkOJp1OOsWJ5E8F5BXTdfRAdDK3fCA&oe=67BD697F" alt="Gallery Image 1">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462561107_3538018906504563_2800419711981788825_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_ohc=c9LbhXVcu_UQ7kNvgFV4vrM&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFfqd5Jx9rpdUwWpVEdcLVC0M0Ny6E455_taRMliosrcQ&oe=67BD882D" alt="Gallery Image 2">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/474044527_1005036784987845_5789323196856802067_n.jpg?stp=dst-jpg_s526x395_tt6&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=0TN5doglixoQ7kNvgFihXxi&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFxW0sI5YZw5DSoRsUCbUyts9UfvzJUrBCSvRfCGezrPw&oe=67BD84F5" alt="Gallery Image 3">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/472833028_596302382984640_1584203616362654151_n.jpg?stp=dst-jpg_s552x414_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=LDDm2FvBYA0Q7kNvgHlkxrH&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gEBlsz9uKIBB5S33_XcXGjKGT3bMhVSGHXR7LvAzCxHHw&oe=67BD618C" alt="Gallery Image 4">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/472291686_1475985676690924_3340391392968174146_n.jpg?stp=dst-jpg_s526x395_tt6&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=DGPFbFp9LoEQ7kNvgF6cNMp&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gEoqWi-fGYDVQfQkjcK7GEIy08hdVhbrW42suVvVVR2bw&oe=67BD8AE6" alt="Gallery Image 5">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462580120_551441067616775_7617988833554118446_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=108&ccb=1-7&_nc_sid=0024fc&_nc_ohc=8nNJXsImZl0Q7kNvgH2wkHF&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFjfc93K6esond4g1CFKlPkY8Mb3_nEQVRXagHmmspVew&oe=67BD6A69" alt="Gallery Image 6">
       </div>
-    </div>
-  </section>
-
-  <section id="goals" class="container two-columns">
-    <div class="column">
-      <h2 align="center">HOBBIES & INTERESTS</h2>
-      <ul>
-        <li>Music (1D, Chappell Roan, Olivia Rodrigo, SZA)</li>
-        <li>Films (Beautiful Boy, Challengers, Pitch Perfect Trilogy)</li>
-        <li>Shows (Bojack Horseman, Grey’s Anatomy, The Bear)</li>
-        <li>Video Games (Life is Strange, The Last of Us, Valorant)</li>
-        <li>Photography</li>
-      </ul>
-    </div>
-    <div class="column">
-      <h2 align="center">GOALS IN LIFE / DREAMS</h2>
-      <ul>
-        <li>To be able to live on my own and give back to my siblings</li>
-        <li>To be able to meet my favorite artists</li>
-        <li>Graduate college with Honors</li>
-        <li>Learn to play the piano</li>
-        <li>Find a stable job that I enjoy</li>
-        <li>Be able to drive without getting anxious</li>
-        <li>To live near a beach and be able to travel all around the world</li>
-      </ul>
-    </div>
-  </section>
-
-  <section id="gallery" class="container">
-    <h2 align="center">PICTURE GALLERY</h2>
-    <div class="gallery">
-      <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462577563_1221159922506811_5912691345356275466_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=FgwusuDZknsQ7kNvgH7m_Sw&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFS1l0vDk20hJ_-SkOJp1OOsWJ5E8F5BXTdfRAdDK3fCA&oe=67BD697F" alt="Gallery Image 1">
-      <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462561107_3538018906504563_2800419711981788825_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_ohc=c9LbhXVcu_UQ7kNvgFV4vrM&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFfqd5Jx9rpdUwWpVEdcLVC0M0Ny6E455_taRMliosrcQ&oe=67BD882D" alt="Gallery Image 2">
-      <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/474044527_1005036784987845_5789323196856802067_n.jpg?stp=dst-jpg_s526x395_tt6&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=0TN5doglixoQ7kNvgFihXxi&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFxW0sI5YZw5DSoRsUCbUyts9UfvzJUrBCSvRfCGezrPw&oe=67BD84F5" alt="Gallery Image 3">
-      <img src="blob:https://www.messenger.com/db6e5283-9dae-4b6b-b625-36febc7a8a6b" alt="Gallery Image 4">
-      <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/472291686_1475985676690924_3340391392968174146_n.jpg?stp=dst-jpg_s526x395_tt6&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=DGPFbFp9LoEQ7kNvgF6cNMp&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gEoqWi-fGYDVQfQkjcK7GEIy08hdVhbrW42suVvVVR2bw&oe=67BD8AE6" alt="Gallery Image 5">
-      <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462580120_551441067616775_7617988833554118446_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=108&ccb=1-7&_nc_sid=0024fc&_nc_ohc=8nNJXsImZl0Q7kNvgH2wkHF&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFjfc93K6esond4g1CFKlPkY8Mb3_nEQVRXagHmmspVew&oe=67BD6A69" alt="Gallery Image 6">
-    </div>
-  </section>  
-
-</body>
+    </section>  
+  </div>
 </template>
 
 <style>
@@ -132,7 +129,7 @@
     }
     
     .navbar .logo {
-      max-width: 50px; 
+      max-width: 80px; 
       margin-right: auto; 
     }
     
